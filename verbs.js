5769eb3160a0d73c508051b1187882090d04417c
Developer <mschuh@meo-tech.de>
2014-12-17 09:56:09 +0100

    new Verbs

--- verbs.js
+++ verbs.js
@@ -99,6 +99,482 @@
       "voided" : {
          "id" : "http://adlnet.gov/expapi/verbs/voided",
          "display" : {"en-US" : "voided"}
+      },
+      "accepted" : {
+         "id": "http://activitystrea.ms/schema/1.0/accept",
+         "display" : {"en-US" : "accepted"}
+      },
+      "accessed" : {
+         "id": "http://activitystrea.ms/schema/1.0/access",
+         "display" : {"en-US" : "accessed"}
+      },
+      "acknowledged" : {
+         "id": "http://activitystrea.ms/schema/1.0/acknowledge",
+         "display" : {"en-US" : "acknowledged"}
+      },
+      "added" : {
+         "id": "http://activitystrea.ms/schema/1.0/add",
+         "display" : {"en-US" : "added"}
+      },
+      "agreed" : {
+         "id": "http://activitystrea.ms/schema/1.0/agree",
+         "display" : {"en-US" : "agreed"}
+      },
+      "appended" : {
+         "id": "http://activitystrea.ms/schema/1.0/append",
+         "display" : {"en-US" : "appended"}
+      },
+      "approved" : {
+         "id": "http://activitystrea.ms/schema/1.0/approve",
+         "display" : {"en-US" : "approved"}
+      },
+      "archived" : {
+         "id": "http://activitystrea.ms/schema/1.0/archive",
+         "display" : {"en-US" : "archived"}
+      },
+      "assigned" : {
+         "id": "http://activitystrea.ms/schema/1.0/assign",
+         "display" : {"en-US" : "accepted"}
+      },
+      "was at" : {
+         "id": "http://activitystrea.ms/schema/1.0/at",
+         "display" : {"en-US" : "was at"}
+      },
+      "attached" : {
+         "id": "http://activitystrea.ms/schema/1.0/attach",
+         "display" : {"en-US" : "attached"}
+      },
+      "attended" : {
+         "id": "http://activitystrea.ms/schema/1.0/attend",
+         "display" : {"en-US" : "attended"}
+      },
+      "authored" : {
+         "id": "http://activitystrea.ms/schema/1.0/author",
+         "display" : {"en-US" : "authored"}
+      },
+      "authorized" : {
+         "id": "http://activitystrea.ms/schema/1.0/authorize",
+         "display" : {"en-US" : "authorized"}
+      },
+      "borrowed" : {
+         "id": "http://activitystrea.ms/schema/1.0/borrow",
+         "display" : {"en-US" : "borrowed"}
+      },
+      "built" : {
+         "id": "http://activitystrea.ms/schema/1.0/build",
+         "display" : {"en-US" : "built"}
+      },
+      "canceled" : {
+         "id": "http://activitystrea.ms/schema/1.0/cancel",
+         "display" : {"en-US" : "canceled"}
+      },
+      "checked in" : {
+         "id": "http://activitystrea.ms/schema/1.0/checkin",
+         "display" : {"en-US" : "checked in"}
+      },
+      "closed" : {
+         "id": "http://activitystrea.ms/schema/1.0/close",
+         "display" : {"en-US" : "closed"}
+      },
+      "completed" : {
+         "id": "http://activitystrea.ms/schema/1.0/complete",
+         "display" : {"en-US" : "completed"}
+      },
+      "confirmed" : {
+         "id": "http://activitystrea.ms/schema/1.0/confirm",
+         "display" : {"en-US" : "confirmed"}
+      },
+      "consumed" : {
+         "id": "http://activitystrea.ms/schema/1.0/consume",
+         "display" : {"en-US" : "consumed"}
+      },
+      "created" : {
+         "id": "http://activitystrea.ms/schema/1.0/create",
+         "display" : {"en-US" : "created"}
+      },
+      "deleted" : {
+         "id": "http://activitystrea.ms/schema/1.0/delete",
+         "display" : {"en-US" : "deleted"}
+      },
+      "delivered" : {
+         "id": "http://activitystrea.ms/schema/1.0/deliver",
+         "display" : {"en-US" : "delivered"}
+      },
+      "denied" : {
+         "id": "http://activitystrea.ms/schema/1.0/deny",
+         "display" : {"en-US" : "denied"}
+      },
+      "disagreed" : {
+         "id": "http://activitystrea.ms/schema/1.0/disagree",
+         "display" : {"en-US" : "disagreed"}
+      },
+      "disliked" : {
+         "id": "http://activitystrea.ms/schema/1.0/dislike",
+         "display" : {"en-US" : "disliked"}
+      },
+      "experienced" : {
+         "id": "http://activitystrea.ms/schema/1.0/experience",
+         "display" : {"en-US" : "experienced"}
+      },
+      "favorited" : {
+         "id": "http://activitystrea.ms/schema/1.0/favorite",
+         "display" : {"en-US" : "favorited"}
+      },
+      "found" : {
+         "id": "http://activitystrea.ms/schema/1.0/find",
+         "display" : {"en-US" : "found"}
+      },
+      "flagged as inappropriate" : {
+         "id": "http://activitystrea.ms/schema/1.0/flag-as-inappropriate",
+         "display" : {"en-US" : "flagged as inappropriate"}
+      },
+      "followed" : {
+         "id": "http://activitystrea.ms/schema/1.0/follow",
+         "display" : {"en-US" : "followed"}
+      },
+      "gave" : {
+         "id": "http://activitystrea.ms/schema/1.0/give",
+         "display" : {"en-US" : "gave"}
+      },
+      "hosted" : {
+         "id": "http://activitystrea.ms/schema/1.0/host",
+         "display" : {"en-US" : "hosted"}
+      },
+      "ignored" : {
+         "id": "http://activitystrea.ms/schema/1.0/ignore",
+         "display" : {"en-US" : "ignored"}
+      },
+      "inserted" : {
+         "id": "http://activitystrea.ms/schema/1.0/insert",
+         "display" : {"en-US" : "inserted"}
+      },
+      "installed" : {
+         "id": "http://activitystrea.ms/schema/1.0/install",
+         "display" : {"en-US" : "installed"}
+      },
+      "interacted" : {
+         "id": "http://activitystrea.ms/schema/1.0/interact",
+         "display" : {"en-US" : "interacted"}
+      },
+      "invited" : {
+         "id": "http://activitystrea.ms/schema/1.0/invite",
+         "display" : {"en-US" : "invited"}
+      },
+      "joined" : {
+         "id": "http://activitystrea.ms/schema/1.0/join",
+         "display" : {"en-US" : "joined"}
+      },
+      "left" : {
+         "id": "http://activitystrea.ms/schema/1.0/leave",
+         "display" : {"en-US" : "left"}
+      },
+      "liked" : {
+         "id": "http://activitystrea.ms/schema/1.0/like",
+         "display" : {"en-US" : "liked"}
+      },
+      "listened" : {
+         "id": "http://activitystrea.ms/schema/1.0/listen",
+         "display" : {"en-US" : "listened"}
+      },
+      "lost" : {
+         "id": "http://activitystrea.ms/schema/1.0/lose",
+         "display" : {"en-US" : "lost"}
+      },
+      "made friend" : {
+         "id": "http://activitystrea.ms/schema/1.0/make-friend",
+         "display" : {"en-US" : "made friend"}
+      },
+      "opened" : {
+         "id": "http://activitystrea.ms/schema/1.0/open",
+         "display" : {"en-US" : "opened"}
+      },
+      "played" : {
+         "id": "http://activitystrea.ms/schema/1.0/play",
+         "display" : {"en-US" : "played"}
+      },
+      "presented" : {
+         "id": "http://activitystrea.ms/schema/1.0/present",
+         "display" : {"en-US" : "presented"}
+      },
+      "purchased" : {
+         "id": "http://activitystrea.ms/schema/1.0/purchase",
+         "display" : {"en-US" : "purchased"}
+      },
+      "qualified" : {
+         "id": "http://activitystrea.ms/schema/1.0/qualify",
+         "display" : {"en-US" : "qualified"}
+      },
+      "read" : {
+         "id": "http://activitystrea.ms/schema/1.0/read",
+         "display" : {"en-US" : "read"}
+      },
+      "received" : {
+         "id": "http://activitystrea.ms/schema/1.0/receive",
+         "display" : {"en-US" : "received"}
+      },
+      "rejected" : {
+         "id": "http://activitystrea.ms/schema/1.0/reject",
+         "display" : {"en-US" : "rejected"}
+      },
+      "removed" : {
+         "id": "http://activitystrea.ms/schema/1.0/remove",
+         "display" : {"en-US" : "removed"}
+      },
+      "removed friend" : {
+         "id": "http://activitystrea.ms/schema/1.0/remove-friend",
+         "display" : {"en-US" : "removed friend"}
+      },
+      "replaced" : {
+         "id": "http://activitystrea.ms/schema/1.0/replace",
+         "display" : {"en-US" : "replaced"}
+      },
+      "requested" : {
+         "id": "http://activitystrea.ms/schema/1.0/request",
+         "display" : {"en-US" : "requested"}
+      },
+      "requested friend" : {
+         "id": "http://activitystrea.ms/schema/1.0/request-friend",
+         "display" : {"en-US" : "requested friend"}
+      },
+      "resolved" : {
+         "id": "http://activitystrea.ms/schema/1.0/resolve",
+         "display" : {"en-US" : "resolved"}
+      },
+      "retracted" : {
+         "id": "http://activitystrea.ms/schema/1.0/retract",
+         "display" : {"en-US" : "retracted"}
+      },
+      "returned" : {
+         "id": "http://activitystrea.ms/schema/1.0/return",
+         "display" : {"en-US" : "returned"}
+      },
+      "RSVPed maybe" : {
+         "id": "http://activitystrea.ms/schema/1.0/rsvp-maybe",
+         "display" : {"en-US" : "RSVPed maybe"}
+      },
+      "RSVPed no" : {
+         "id": "http://activitystrea.ms/schema/1.0/rsvp-no",
+         "display" : {"en-US" : "RSVPed no"}
+      },
+      "RSVPed yes" : {
+         "id": "http://activitystrea.ms/schema/1.0/rsvp-yes",
+         "display" : {"en-US" : "RSVPed yes"}
+      },
+      "satisfied" : {
+         "id": "http://activitystrea.ms/schema/1.0/satisfy",
+         "display" : {"en-US" : "satisfied"}
+      },
+      "saved" : {
+         "id": "http://activitystrea.ms/schema/1.0/save",
+         "display" : {"en-US" : "saved"}
+      },
+      "scheduled" : {
+         "id": "http://activitystrea.ms/schema/1.0/schedule",
+         "display" : {"en-US" : "scheduled"}
+      },
+      "searched" : {
+         "id": "http://activitystrea.ms/schema/1.0/search",
+         "display" : {"en-US" : "searched"}
+      },
+      "sold" : {
+         "id": "http://activitystrea.ms/schema/1.0/sell",
+         "display" : {"en-US" : "sold"}
+      },
+      "sent" : {
+         "id": "http://activitystrea.ms/schema/1.0/send",
+         "display" : {"en-US" : "sent"}
+      },
+      "shared" : {
+         "id": "http://activitystrea.ms/schema/1.0/share",
+         "display" : {"en-US" : "shared"}
+      },
+      "sponsored" : {
+         "id": "http://activitystrea.ms/schema/1.0/sponsor",
+         "display" : {"en-US" : "sponsored"}
+      },
+      "started" : {
+         "id": "http://activitystrea.ms/schema/1.0/start",
+         "display" : {"en-US" : "started"}
+      },
+      "stopped following" : {
+         "id": "http://activitystrea.ms/schema/1.0/stop-following",
+         "display" : {"en-US" : "stopped following"}
+      },
+      "submitted" : {
+         "id": "http://activitystrea.ms/schema/1.0/submit",
+         "display" : {"en-US" : "submitted"}
+      },
+      "tagged" : {
+         "id": "http://activitystrea.ms/schema/1.0/tag",
+         "display" : {"en-US" : "tagged"}
+      },
+      "terminated" : {
+         "id": "http://activitystrea.ms/schema/1.0/terminate",
+         "display" : {"en-US" : "terminated"}
+      },
+      "tied" : {
+         "id": "http://activitystrea.ms/schema/1.0/tie",
+         "display" : {"en-US" : "tied"}
+      },
+      "unfavorited" : {
+         "id": "http://activitystrea.ms/schema/1.0/unfavorite",
+         "display" : {"en-US" : "unfavorited"}
+      },
+      "unliked" : {
+         "id": "http://activitystrea.ms/schema/1.0/unlike",
+         "display" : {"en-US" : "unliked"}
+      },
+      "unsatisfied" : {
+         "id": "http://activitystrea.ms/schema/1.0/unsatisfy",
+         "display" : {"en-US" : "unsatisfied"}
+      },
+      "unsaved" : {
+         "id": "http://activitystrea.ms/schema/1.0/unsave",
+         "display" : {"en-US" : "unsaved"}
+      },
+      "unshared" : {
+         "id": "http://activitystrea.ms/schema/1.0/unshare",
+         "display" : {"en-US" : "unshared"}
+      },
+      "updated" : {
+         "id": "http://activitystrea.ms/schema/1.0/update",
+         "display" : {"en-US" : "updated"}
+      },
+      "used" : {
+         "id": "http://activitystrea.ms/schema/1.0/use",
+         "display" : {"en-US" : "used"}
+      },
+      "watched" : {
+         "id": "http://activitystrea.ms/schema/1.0/watch",
+         "display" : {"en-US" : "watched"}
+      },
+      "won" : {
+         "id": "http://activitystrea.ms/schema/1.0/win",
+         "display" : {"en-US" : "won"}
+      },
+      "pressed" : {
+         "id": "http://future-learning.info/xAPI/verb/pressed",
+         "display" : {"en-US" : "pressed"}
+      },
+      "released" : {
+         "id": "http://future-learning.info/xAPI/verb/released",
+         "display" : {"en-US" : "released"}
+      },
+      "bookmarked" : {
+         "id": "http://id.tincanapi.com/verb/bookmarked",
+         "display" : {"en-US" : "bookmarked"}
+      },
+      "called" : {
+         "id": "http://id.tincanapi.com/verb/called",
+         "display" : {"en-US" : "called"}
+      },
+      "closed sale" : {
+         "id": "http://id.tincanapi.com/verb/closed-sale",
+         "display" : {"en-US" : "closed sale"}
+      },
+      "created opportunity" : {
+         "id": "http://id.tincanapi.com/verb/created-opportunity",
+         "display" : {"en-US" : "created opportunity"}
+      },
+      "focused" : {
+         "id": "http://id.tincanapi.com/verb/focused",
+         "display" : {"en-US" : "focused"}
+      },
+      "entered frame" : {
+         "id": "http://id.tincanapi.com/verb/frame/entered",
+         "display" : {"en-US" : "entered frame"}
+      },
+      "exited frame" : {
+         "id": "http://id.tincanapi.com/verb/frame/exited",
+         "display" : {"en-US" : "exited frame"}
+      },
+      "hired" : {
+         "id": "http://id.tincanapi.com/verb/hired",
+         "display" : {"en-US" : "hired"}
+      },
+      "interviewed" : {
+         "id": "http://id.tincanapi.com/verb/interviewed",
+         "display" : {"en-US" : "interviewed"}
+      },
+      "mentored" : {
+         "id": "http://id.tincanapi.com/verb/mentored",
+         "display" : {"en-US" : "mentored"}
+      },
+      "paused" : {
+         "id": "http://id.tincanapi.com/verb/paused",
+         "display" : {"en-US" : "paused"}
+      },
+      "rated" : {
+         "id": "http://id.tincanapi.com/verb/rated",
+         "display" : {"en-US" : "rated"}
+      },
+      "replied to tweet" : {
+         "id": "http://id.tincanapi.com/verb/replied-to-tweet",
+         "display" : {"en-US" : "replied to tweet"}
+      },
+      "requested attention" : {
+         "id": "http://id.tincanapi.com/verb/requested-attention",
+         "display" : {"en-US" : "requested attention"}
+      },
+      "retweeted" : {
+         "id": "http://id.tincanapi.com/verb/retweeted",
+         "display" : {"en-US" : "retweeted"}
+      },
+      "reviewed" : {
+         "id": "http://id.tincanapi.com/verb/reviewed",
+         "display" : {"en-US" : "reviewed"}
+      },
+      "skipped" : {
+         "id": "http://id.tincanapi.com/verb/skipped",
+         "display" : {"en-US" : "skipped"}
+      },
+      "tweeted" : {
+         "id": "http://id.tincanapi.com/verb/tweeted",
+         "display" : {"en-US" : "tweeted"}
+      },
+      "unfocused" : {
+         "id": "http://id.tincanapi.com/verb/unfocused",
+         "display" : {"en-US" : "unfocused"}
+      },
+      "viewed" : {
+         "id": "http://id.tincanapi.com/verb/viewed",
+         "display" : {"en-US" : "viewed"}
+      },
+      "down voted" : {
+         "id": "http://id.tincanapi.com/verb/voted-down",
+         "display" : {"en-US" : "down voted"}
+      },
+      "up voted" : {
+         "id": "http://id.tincanapi.com/verb/voted-up",
+         "display" : {"en-US" : "up voted"}
+      },
+      "annotated" : {
+         "id": "http://risc-inc.com/annotator/verbs/annotated",
+         "display" : {"en-US" : "annotated"}
+      },
+      "modified annotation" : {
+         "id": "http://risc-inc.com/annotator/verbs/modified",
+         "display" : {"en-US" : "modified annotation"}
+      },
+      "drew" : {
+         "id": "http://www.digital-knowledge.co.jp/tincanapi/verbs/drew",
+         "display" : {"en-US" : "drew"}
+      },
+      "cancelled planned learning" : {
+         "id": "http://www.tincanapi.co.uk/pages/verbs.html#cancelled_planned_learning",
+         "display" : {"en-US" : "cancelled planned learning"}
+      },
+      "planned" : {
+         "id": "http://www.tincanapi.co.uk/pages/verbs.html#planned_learning",
+         "display" : {"en-US" : "planned"}
+      },
+      "enrolled onto learning plan" : {
+         "id": "http://www.tincanapi.co.uk/verbs/enrolled_onto_learning_plan",
+         "display" : {"en-US" : "enrolled onto learning plan"}
+      },
+      "evaluated" : {
+         "id": "http://www.tincanapi.co.uk/verbs/evaluated",
+         "display" : {"en-US" : "evaluated"}
       }
    };
 }(window.ADL = window.ADL || {}));
